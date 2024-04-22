c242c250a70872dbfe5f3a99cfa8fdb8c531cf36
Moisés David Jiménez Ortiz <143739485+MJoDev@users.noreply.github.com>
2024-04-22 17:37:38 -0400

    4

--- facerecognition.py
+++ facerecognition.py
@@ -28,6 +28,7 @@ ANC_PATH = os.path.join('data', 'anchor')
 
 
 #Establish a connection to the webcam
+
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
     ret, frame = cap.read()
@@ -56,9 +57,9 @@ while cap.isOpened():
 cap.release()
 cv2.destroyAllWindows()
 
-anchor = tf.data.Dataset.list_files(ANC_PATH+'\.jpg').take(300)
-positive = tf.data.Dataset.list_files(POS_PATH+'\.jpg').take(300)
-negative = tf.data.Dataset.list_files(NEG_PATH+'\.jpg').take(300)
+anchor = tf.data.Dataset.list_files(ANC_PATH+'\*.jpg').take(300)
+positive = tf.data.Dataset.list_files(POS_PATH+'\*.jpg').take(300)
+negative = tf.data.Dataset.list_files(NEG_PATH+'\*.jpg').take(300)
 
 def preprocess(file_path):
     #Reading image for file path
@@ -78,3 +79,50 @@ data = positives.concatenate(negatives)
 
 def preprocess_twin(input_img, validation_img, label):
     return(preprocess(input_img), preprocess(validation_img), label)
+
+data = data.map(preprocess_twin)
+data = data.cache()
+data = data.shuffle(buffer_size=1024)
+
+#Training partition
+train_data = data.take(round(len(data)*.7))
+train_data = train_data.batch(16)
+train_data = train_data.prefetch(8)
+
+test_data = data.skip(round(len(data)*.7))
+test_data = test_data.take(round(len(data)*.3))
+test_data = test_data.batch(16)
+test_data = test_data.prefetch(8)
+
+def make_embedding():
+    inp = Input(shape=(100,100, 3), name='input_image')
+    #First Block
+    c1 = Conv2D(64, (10,10), activation='relu')(inp)
+    m1 = MaxPooling2D(64, (2,2), padding='same')(c1)
+    #Second block
+    c2 = Conv2D(128, (7,7), activation='relu')(m1)
+    m2 = MaxPooling2D(64, (2,2), padding='same')(c2)
+    #Third block
+    c3 = Conv2D(128, (4,4), activation='relu')(m2)
+    m3 = MaxPooling2D(64, (2,2), padding='same')(c3)
+    #Final block
+    c4 = Conv2D(256, (4, 4), activation='relu')(m3)
+    f1 = Flatten()(c4)
+    d1 = Dense(4096, activation='sigmoid')(f1)
+
+    return Model(inputs=[inp], outputs=[d1], name='embedding')
+
+model = make_embedding()
+
+model.summary()
+
+# Siamese L1 Distance class
+class L1Dist(Layer):
+    
+    # Init method - inheritance
+    def __init__(self, **kwargs):
+        super().__init__()
+       
+    # Magic happens here - similarity calculation
+    def call(self, input_embedding, validation_embedding):
+        return tf.math.abs(input_embedding - validation_embedding)

a39b50b2704f97cf2b60f7ef2ed81d03cbd3492b
Moisés David Jiménez Ortiz <143739485+MJoDev@users.noreply.github.com>
2024-04-23 11:43:27 -0400

    FINAL

--- facerecognition.py
+++ facerecognition.py
@@ -1,128 +1,53 @@
-#import standard dependencies
+import threading
+
 import cv2
-import os
-import random 
-import numpy as np
-from matplotlib import pyplot as plt
+from deepface import DeepFace
 
-#import tensorflow dependecies - Funtional API
-from tensorflow import keras
-from keras.layers import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
-from keras.models import Model 
-import tensorflow as tf
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-#import uuid library to generate unique image names
-import uuid
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+counter = 0
+face_match = False
 
-# Avoid OOM errors
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
+reference_img = cv2.imread("reference.png")
 
-#Setup Paths
-
-POS_PATH = os.path.join('data', 'positive')
-NEG_PATH = os.path.join('data', 'negative')
-ANC_PATH = os.path.join('data', 'anchor')
+def check_face(frame):
+    global face_match
+    try: 
+        if DeepFace.verify(frame, reference_img.copy())['verified']:
+            face_match = True
+        else:
+            face_match = False
+    except ValueError:
+        face_match = False
 
 
-#Establish a connection to the webcam
+while True:
 
-cap = cv2.VideoCapture(0)
-while cap.isOpened():
     ret, frame = cap.read()
 
-    #Establish 250
-    frame = frame[80:80+250, 200:200+250, :]
+    if ret:
+        if counter % 30 == 0:
+            try:
+                threading.Thread(target=check_face, args=(frame.copy(),)).start()
+            except ValueError:  
+                pass
+        counter += 1
 
-    #Collect anchors
-    if cv2.waitKey(1) & 0XFF == ord('a'):
-        #Creating a unique file name
-        imgname = os.path.join(ANC_PATH, '{}.jpg'.format(uuid.uuid1()))
-        # Write out anchor image
-        cv2.imwrite(imgname, frame)
-    #Collect positives
-    if cv2.waitKey(1) & 0XFF == ord('p'):
-        #Creating a unique file name
-        imgname = os.path.join(POS_PATH, '{}.jpg'.format(uuid.uuid1()))
-        # Write out positive image
-        cv2.imwrite(imgname, frame)
-    #Show image back to screen
-    cv2.imshow('Image Collection', frame)
 
-    #Breaking gracefully
-    if cv2.waitKey(1) & 0XFF == ord('q'):
+        if face_match:
+            cv2.putText(frame, "Match", (20,450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+        else:
+            cv2.putText(frame, "No Match", (20,450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+
+        cv2.imshow("Video", frame)
+
+
+
+    key = cv2.waitKey(1)
+    if key == ord("q"):
         break
-cap.release()
+
 cv2.destroyAllWindows()
-
-anchor = tf.data.Dataset.list_files(ANC_PATH+'\*.jpg').take(300)
-positive = tf.data.Dataset.list_files(POS_PATH+'\*.jpg').take(300)
-negative = tf.data.Dataset.list_files(NEG_PATH+'\*.jpg').take(300)
-
-def preprocess(file_path):
-    #Reading image for file path
-    byte_img = tf.io.read_file(file_path)
-    # Load the image
-    img = tf.io.decode_jpeg(byte_img)
-
-    #Processing steps - resizing image
-    img = tf.image.resize(img, (100, 100))
-    img = img / 255.0
-    return img
-
-positives = tf.data.Dataset.zip((anchor, positive, tf.data.Dataset.from_tensor_slices(tf.ones(len(anchor)))))
-negatives = tf.data.Dataset.zip((anchor, negative, tf.data.Dataset.from_tensor_slices(tf.zeros(len(anchor)))))
-
-data = positives.concatenate(negatives)
-
-def preprocess_twin(input_img, validation_img, label):
-    return(preprocess(input_img), preprocess(validation_img), label)
-
-data = data.map(preprocess_twin)
-data = data.cache()
-data = data.shuffle(buffer_size=1024)
-
-#Training partition
-train_data = data.take(round(len(data)*.7))
-train_data = train_data.batch(16)
-train_data = train_data.prefetch(8)
-
-test_data = data.skip(round(len(data)*.7))
-test_data = test_data.take(round(len(data)*.3))
-test_data = test_data.batch(16)
-test_data = test_data.prefetch(8)
-
-def make_embedding():
-    inp = Input(shape=(100,100, 3), name='input_image')
-    #First Block
-    c1 = Conv2D(64, (10,10), activation='relu')(inp)
-    m1 = MaxPooling2D(64, (2,2), padding='same')(c1)
-    #Second block
-    c2 = Conv2D(128, (7,7), activation='relu')(m1)
-    m2 = MaxPooling2D(64, (2,2), padding='same')(c2)
-    #Third block
-    c3 = Conv2D(128, (4,4), activation='relu')(m2)
-    m3 = MaxPooling2D(64, (2,2), padding='same')(c3)
-    #Final block
-    c4 = Conv2D(256, (4, 4), activation='relu')(m3)
-    f1 = Flatten()(c4)
-    d1 = Dense(4096, activation='sigmoid')(f1)
-
-    return Model(inputs=[inp], outputs=[d1], name='embedding')
-
-model = make_embedding()
-
-model.summary()
-
-# Siamese L1 Distance class
-class L1Dist(Layer):
-    
-    # Init method - inheritance
-    def __init__(self, **kwargs):
-        super().__init__()
-       
-    # Magic happens here - similarity calculation
-    def call(self, input_embedding, validation_embedding):
-        return tf.math.abs(input_embedding - validation_embedding)
